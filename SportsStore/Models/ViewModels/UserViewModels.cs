7269532dd9b1f01dcd129c2fd6a487ef85ca941c
katlegomaphango <mphangokatlego08@gmail.com>
2023-11-28 13:35:05 +0200

    user view models

---
+++ SportsStore/Models/ViewModels/UserViewModels.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models.ViewModels;
+
+public class LoginModel
+{
+    [Required]
+    [UIHint("email")]
+    public string Email { get; set; }
+
+    [Required]
+    [UIHint("password")]
+    public string Password { get; set; }
+
+    public string ReturnUrl { get; set; } = "/";
+}
+
+public class RegisterModel
+{
+    [Required]
+    [Display(Name = "Username")]
+    public string UserName { get; set; }
+
+    [Required]
+    [Display(Name = "Email")]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name = "Password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "Password must mathc!!!")]
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
