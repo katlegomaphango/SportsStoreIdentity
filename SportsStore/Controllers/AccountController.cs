533f2a78474d40066b607c099217d22bbd07b5ed
katlegomaphango <mphangokatlego08@gmail.com>
2023-11-28 13:22:17 +0200

    initial acc controler

---
+++ SportsStore/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signIn, UserManager<IdentityUser> user)
+        {
+            _signInManager = signIn;
+            _userManager = user;
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
