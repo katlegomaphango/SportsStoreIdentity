7471c103a73e87e8e4d4ce146803ebb06103fa92
katlegomaphango <mphangokatlego08@gmail.com>
2023-11-28 14:12:48 +0200

    login and regitser methods

--- SportsStore/Controllers/AccountController.cs
+++ SportsStore/Controllers/AccountController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -16,9 +16,76 @@ namespace SportsStore.Controllers
         }
 
 
-        public ActionResult Login()
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl,
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user,
+                        model.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return Redirect(model?.ReturnUrl ?? "Home/Index/");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid email or password");
+            return View(model);
+        }
+
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel registerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = registerModel.UserName,
+                    Email = registerModel.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, registerModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                } 
+                else
+                {
+                    foreach (var error in result.Errors.Select(x => x.Description))
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(registerModel);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
 
     }
